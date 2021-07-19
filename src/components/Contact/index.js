d974ecd37b5f5559ac5f616e4ee3a808a0a1e6b3
katredford <katharine.redford@gmail.com>
2021-07-19 18:31:08 -0500

    contact form

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,22 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Contact() {
+	const [formState, setFormState] = useState({
+		name: "",
+		email: "",
+		message: "",
+	});
+	const { name, email, message } = formState;
+
+	function handleChange(e) {
+		setFormState({ ...formState, [e.target.name]: e.target.value });
+	}
+
+	// console.log(formState);
+
 	return (
 		<section>
 			<h1>Contact me</h1>
 			<form id="contact-form">
 				<div>
 					<label htmlFor="name">Name:</label>
-					<input type="text" name="name" />
+					<input
+						type="text"
+						name="name"
+						defaultValue={name}
+						onChange={handleChange}
+						name="name"
+					/>
 				</div>
 				<div>
 					<label htmlFor="email">Email address:</label>
-					<input type="email" name="email" />
+					<input
+						type="email"
+						name="email"
+						defaultValue={email}
+						name="email"
+						onChange={handleChange}
+					/>
 				</div>
 				<div>
 					<label htmlFor="message">Message:</label>
-					<textarea name="message" rows="5" />
+					<textarea
+						name="message"
+						defaultValue={message}
+						onChange={handleChange}
+						rows="5"
+					/>
 				</div>
+				<form id="contact-form" onSubmit={handleSubmit}></form>
 				<button type="submit">Submit</button>
 			</form>
 		</section>
